b9f67a0306b4d0adffd20e481c928ab818f96bb9
ConnyYue <connyyue@gmail.com>
2020-06-28 18:00:50 +0800

    准备开始容器组件；

---
+++ 3、容器组件/flutter_demo_vs/lib/FTHomePage.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+
+class FTHomePage extends StatefulWidget {
+  FTHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _FTHomePageState createState() => _FTHomePageState();
+}
+
+class _FTHomePageState extends State<FTHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            RaisedButton(
+              child: Text(
+                "Column",
+                style: TextStyle(fontSize: 26.0, color: Colors.blue),
+              ),
+              onPressed: () {
+                print("onPressed:");
+              },
+            ),
+            RaisedButton(
+              child: Text(
+                "Flex",
+                style: TextStyle(fontSize: 26.0, color: Colors.blue),
+              ),
+              onPressed: () {
+                print("onPressed:");
+              },
+            ),
+            RaisedButton(
+              child: Text(
+                "Flow",
+                style: TextStyle(fontSize: 26.0, color: Colors.blue),
+              ),
+              onPressed: () {
+                print("onPressed:");
+              },
+            ),
+            RaisedButton(
+              child: Text(
+                "Stack",
+                style: TextStyle(fontSize: 26.0, color: Colors.blue),
+              ),
+              onPressed: () {
+                print("onPressed:");
+              },
+            ),
+            RaisedButton(
+              child: Text(
+                "Align",
+                style: TextStyle(fontSize: 26.0, color: Colors.blue),
+              ),
+              onPressed: () {
+                print("onPressed:");
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
